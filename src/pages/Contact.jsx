85282bd479335c9bf62d0e5a7d43afc5d4d364c1
Chaitanya Rai <cr03@Chaitanyas-MacBook-Air.local>
2025-11-25 20:04:36 +0530

    auto commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,6 +4,7 @@ const Contact = () => {
     return(
         <>
             <h1>Contact Page</h1>
+            <p>If you have any questions, feel free to reach out to us at</p>
         </>
     )
 }
