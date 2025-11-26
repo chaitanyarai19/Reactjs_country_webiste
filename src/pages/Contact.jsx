838afff8edfc083d5e5c070f9aebaf48b5b3cbcf
Chaitanya Rai <cr03@Chaitanyas-MacBook-Air.local>
2025-11-26 13:29:09 +0530

    auto commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,8 +3,17 @@ import React from "react";
 const Contact = () => {
     return(
         <>
-            <h1>Contact Page</h1>
-            <p>If you have any questions, feel free to reach out to us at</p>
+            <section className="section-contact container">
+                <h2 className="container-title">
+                Get in Touch with Us
+                </h2>
+                <form action="">
+                    <input type="text" name="name" placeholder="Your Name" autoComplete="false" required/>
+                    <input type="email" name="email" placeholder="Your Email" autoComplete="false" required/>
+                    <textarea name="message" rows="5" placeholder="Your Message" required></textarea>
+                    <button type="submit" className="btn btn-darken btn-inline bg-blue-box">Send Message</button>
+                </form>
+            </section>
         </>
     )
 }
