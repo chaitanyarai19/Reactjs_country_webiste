ffe46f4ae6e45e062709f7e518c8d98b43de0bb2
Chaitanya Rai <cr03@Chaitanyas-MacBook-Air.local>
2025-11-26 13:42:39 +0530

    auto commit

--- src/Components/UI/Footer.jsx
+++ src/Components/UI/Footer.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import footerApi from "../../api/footerApi.json";
 
 const Footer = () => {
     return(
         <>
             <footer className="footer-section">
                 <div className="container grid grid-three-cols">
+                    {
+                        footerApi.map((footer, index) => {
+                            return(
+                                <div className="footer-contact" key={index}>
+                                    <div className="icon">{footer.icon}</div>
+                                    <div className="footer-contact-text">
+                                        <h3 className="footer-title">{footer.title}</h3>
+                                        <p className="footer-subtitle">{footer.subTitle}</p>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </footer>
         </>
