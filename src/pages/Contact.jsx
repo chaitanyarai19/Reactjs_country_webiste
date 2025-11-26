40f001dbb7d8e785fbaa09bf405947a811b58ba6
Chaitanya Rai <cr03@Chaitanyas-MacBook-Air.local>
2025-11-26 13:31:28 +0530

    auto commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,12 +7,14 @@ const Contact = () => {
                 <h2 className="container-title">
                 Get in Touch with Us
                 </h2>
+                <div className="contact-wrapper container">
                 <form action="">
                     <input className="form-control" type="text" name="name" placeholder="Your Name" autoComplete="false" required/>
                     <input className="form-control" type="email" name="email" placeholder="Your Email" autoComplete="false" required/>
                     <textarea className="form-control" name="message" rows="5" placeholder="Your Message" required></textarea>
                     <button type="submit" className="btn btn-darken btn-inline bg-blue-box">Send Message</button>
                 </form>
+                </div>
             </section>
         </>
     )
