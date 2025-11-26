8b4c4c7b0d6eebc5f14c996d1b6a32c9376e886b
Chaitanya Rai <cr03@Chaitanyas-MacBook-Air.local>
2025-11-26 13:51:40 +0530

    auto commit

--- src/Components/UI/Footer.jsx
+++ src/Components/UI/Footer.jsx
@@ -1,4 +1,4 @@
-import footerContact from "../../api/footerApi.json";
+import footerApi from "../../api/footerApi.json";
 import { IoCallSharp } from "react-icons/io5";
 import { MdPlace } from "react-icons/md";
 import { TbMailPlus } from "react-icons/tb";
