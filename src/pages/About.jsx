baff777736228d7c01584e6bd98159400f6876e5
Chaitanya Rai <cr03@Chaitanyas-MacBook-Air.local>
2025-11-26 11:47:38 +0530

    auto commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,32 +1,10 @@
 import React from "react";
 import countryData from "../api/countryData.json";
-
+import HeroSection from "../Components/UI/HeroSection";
 const About = () => {
     return(
         <>
-            <section className="section-about container">
-                Here are the details about our World Atlas website. We provide comprehensive information about countries around the globe, including maps, cultural insights, and interesting facts to help you explore and learn more about the world we live in.
-                <br/>
-                We're dedicated to making geographical knowledge accessible and engaging for everyone.
-
-                <div className="gradient-cards">
-                    {countryData.map((country) => {
-                        return(
-                            <div className="card" key={country.id}>
-                                <div className="container-card bg-blue-box">
-                                    <p className="card-title">{country.countryName}</p>
-                                    <p>
-                                        <span className="card-description">Capital:</span> {country.capital}<br/>
-                                        <span className="card-description">Population:</span> {country.population.toLocaleString()}<br/>
-                                        <span className="card-description">Interesting Fact:</span> {country.interestingFact}
-                                    </p>
-                                </div>
-                            </div>
-                        )
-                    })}
-\
-                </div>
-            </section>
+            <HeroSection/>
         </>
     )
 }
