2ddecae4785911e80482146fbfcbf2c60ee19720
Chaitanya Rai <cr03@Chaitanyas-MacBook-Air.local>
2025-11-26 11:49:10 +0530

    auto commit

--- src/Components/UI/HeroSection.jsx
+++ src/Components/UI/HeroSection.jsx
@@ -2,29 +2,22 @@ import countryData from "../../api/countryData.json";
 
 const HeroSection = () => {
     return(
-        <section className="section-about container">
-                Here are the details about our World Atlas website. We provide comprehensive information about countries around the globe, including maps, cultural insights, and interesting facts to help you explore and learn more about the world we live in.
-                <br/>
-                We're dedicated to making geographical knowledge accessible and engaging for everyone.
-
-                <div className="gradient-cards">
-                    {countryData.map((country) => {
-                        return(
-                            <div className="card" key={country.id}>
-                                <div className="container-card bg-blue-box">
-                                    <p className="card-title">{country.countryName}</p>
-                                    <p>
-                                        <span className="card-description">Capital:</span> {country.capital}<br/>
-                                        <span className="card-description">Population:</span> {country.population.toLocaleString()}<br/>
-                                        <span className="card-description">Interesting Fact:</span> {country.interestingFact}
-                                    </p>
-                                </div>
-                            </div>
-                        )
-                    })}
-\
-                </div>
-            </section>
+            <main className="hero-section main">
+           <div className="container grid grid-two-cols">
+            <div className="hero-content">
+                <h1 className="heading-xl">
+                    Explore the World with Our Comprehensive Atlas  
+                </h1>
+                <p className="paragraph">
+                    Discover detailed maps, country profiles, and cultural insights all in one place. Start your journey today!
+                </p>
+                <button className="btn btn-darken btn-inline bg-white-box">Get Started<FaLongArrowAltRight/></button>
+            </div> 
+            <div className="hero-image">
+                <img src="/images/world.png" alt="world image" className="banner-image"/>
+            </div>
+           </div>
+        </main>
     )
 }         
 
