d31bc409e273883a5a3a8c76208d1464206bb262
Chaitanya Rai <cr03@Chaitanyas-MacBook-Air.local>
2025-11-26 11:48:51 +0530

    auto commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,22 +3,7 @@ import { FaLongArrowAltRight } from "react-icons/fa";
 
 const Home = () => {
     return(
-        <main className="hero-section main">
-           <div className="container grid grid-two-cols">
-            <div className="hero-content">
-                <h1 className="heading-xl">
-                    Explore the World with Our Comprehensive Atlas  
-                </h1>
-                <p className="paragraph">
-                    Discover detailed maps, country profiles, and cultural insights all in one place. Start your journey today!
-                </p>
-                <button className="btn btn-darken btn-inline bg-white-box">Get Started<FaLongArrowAltRight/></button>
-            </div> 
-            <div className="hero-image">
-                <img src="/images/world.png" alt="world image" className="banner-image"/>
-            </div>
-           </div>
-        </main>
+        
     )
 }
 
