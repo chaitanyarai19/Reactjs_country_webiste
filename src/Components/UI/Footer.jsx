8dedb598da583e6698b1df29552f65d542833728
Chaitanya Rai <cr03@Chaitanyas-MacBook-Air.local>
2025-11-26 13:48:06 +0530

    auto commit

--- src/Components/UI/Footer.jsx
+++ src/Components/UI/Footer.jsx
@@ -1,6 +1,10 @@
+import { MdPlace } from "react-icons/md";
 import footerApi from "../../api/footerApi.json";
 
 const Footer = () => {
+    const footerIcon = {
+        MdPlace: <MdPlace/>
+    }
     return(
         <>
             <footer className="footer-section">
@@ -9,7 +13,7 @@ const Footer = () => {
                         footerApi.map((footer, index) => {
                             return(
                                 <div className="footer-contact" key={index}>
-                                    <div className="icon">{footer.icon}</div>
+                                    <div className="icon">{footer.MdPlace}</div>
                                     <div className="footer-contact-text">
                                         <p>{footer.title}</p>
                                         <p>{footer.subTitle}</p>
