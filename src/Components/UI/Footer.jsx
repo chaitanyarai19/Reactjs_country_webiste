fdaa393d943c04d359ccbfa960aefb7918494f84
Chaitanya Rai <cr03@Chaitanyas-MacBook-Air.local>
2025-11-26 13:48:55 +0530

    auto commit

--- src/Components/UI/Footer.jsx
+++ src/Components/UI/Footer.jsx
@@ -1,9 +1,13 @@
 import { MdPlace } from "react-icons/md";
 import footerApi from "../../api/footerApi.json";
+import { TbMailPlus } from "react-icons/tb";
+import { IoCallsharp } from "react-icons/io5";
 
 const Footer = () => {
     const footerIcon = {
-        MdPlace: <MdPlace/>
+        MdPlace: <MdPlace/>,
+        IoCallsharp: <IoCallsharp/>,
+        TbMailPlus: <TbMailPlus/>
     }
     return(
         <>
