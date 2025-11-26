bd200910b19def56689726e0b4df9bda884360a6
Chaitanya Rai <cr03@Chaitanyas-MacBook-Air.local>
2025-11-26 13:33:35 +0530

    auto commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Contact = () => {
     const handleFormSubmit = (formData) => {
         console.log(formData.entries());
+    }
     return(
         <>
             <section className="section-contact container">
