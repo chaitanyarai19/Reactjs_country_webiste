b7d9bf4a46c50a93c83130f07ab65724ae844752
chaitanyarai19 <chaitanyarai19@gmail.com>
2025-11-27 09:30:39 +0530

    src/pages/Country.jsx updated}

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,13 +1,15 @@
-import { useEffect, useTransition } from "react";
+import { useEffect, useState, useTransition } from "react";
 import { getCountryData } from "../api/postApi";
+import Loader from "../Components/UI/Loader";
 
 const Country = () => {
     const [isPending, startTransition] = useTransition();
+    const [countires, setCountries] = useState([]);
 
     useEffect(() => {
        startTransition(async() => {
             const res = await getCountryData();
-            console.log(res.data);
+            setCountries(res.data);
        });
        
     }, []);
