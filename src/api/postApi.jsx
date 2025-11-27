93ac3918d2395f55c4b62d1ea7dc347fb5937990
chaitanyarai19 <chaitanyarai19@gmail.com>
2025-11-27 09:27:37 +0530

    src/api/postApi.jsx updated}

--- src/api/postApi.jsx
+++ src/api/postApi.jsx
@@ -1,5 +1,10 @@
 import axios from "axios";
 
-axios.create({
+const api = axios.create({
     baseURL: "https://restcountries.com/v3.1"
 });
+
+export const getCountryData = () =>{
+    return api.get("/all?fields=name,population,region,capital,flags");
+};
+
