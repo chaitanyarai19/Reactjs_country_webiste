2b66e0b62457499f172e54d69be0ed2de4f0e8cb
chaitanyarai19 <chaitanyarai19@gmail.com>
2025-11-27 08:30:57 +0530

    auto commit - 11/27/2025, 8:30:57 AM

--- src/Components/UI/Footer.jsx
+++ src/Components/UI/Footer.jsx
@@ -27,6 +27,48 @@ const Footer = () => {
                         })
                     }
                 </div>
+                <div className="copyright-area">
+        <div className="container">
+          <div className="grid grid-two-cols">
+            <div className="copyright-text">
+              <p>
+                Copyright &copy; 2024, All Right Reserved
+                <NavLink to="https://thapatechnical.shop/" target="_blank">
+                  ThapaTechnical
+                </NavLink>
+              </p>
+            </div>
+
+            <div className="footer-menu">
+              <ul>
+                <li>
+                  <NavLink to="/">Home</NavLink>
+                </li>
+
+                <li>
+                  <NavLink
+                    to="https://www.instagram.com/thapatechnical/"
+                    target="_blank"
+                  >
+                    Social
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="https://thapatechnical.shop/source-code"
+                    target="_blank"
+                  >
+                    Source Code
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/contact">Contact</NavLink>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
             </footer>
         </>
     )
